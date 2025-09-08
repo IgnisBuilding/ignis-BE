c32b3ad1c0f9998c88f2e824fe4bb7aafc82aeaf
rizz_29 <ar.raza5092@gmail.com>
2025-09-09 03:55:15 +0500

    notifications app and entities added

---
+++ apps/notifications/src/entity/notification.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class ammenity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    user_id: string;
+
+    @Column()
+    type: string;
+
+    @Column({ type: 'date'})
+    message: string;
+
+    @Column({ type: 'date'})
+    status: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    created_at: Date;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+    updated_at: Date;
+}  
